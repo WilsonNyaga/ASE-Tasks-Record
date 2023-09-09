d7fd688873d2f2754738038a4123ec80cc1d3751
Willy_N <56235429+WilsonNyaga@users.noreply.github.com>
2023-09-09 14:30:56 +0300

    Update forms.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -6,3 +6,12 @@ class ContactForm(forms.Form):
     message = forms.CharField(widget=forms.Textarea)
     sender = forms.EmailField()
     cc_myself = forms.BooleanField(required=False)
+
+# myapp/forms.py
+from django import forms
+from .models import Aircraft  # Import the Aircraft model from models.py
+
+class AircraftForm(forms.ModelForm):
+    class Meta:
+        model = Aircraft  # Specify the model for the form
+        fields = ['registration', 'client', 'job_number', 'date']  # Define the fields you want in the form
